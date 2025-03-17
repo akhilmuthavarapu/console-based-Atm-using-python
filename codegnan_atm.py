730745f0546bcf6b4054073bc268dfe66c5e0d96
MUTHAVARAPU VENKATA AKHIL <118909448+akhilmuthavarapu@users.noreply.github.com>
2025-03-17 22:31:47 +0530

    Update codegnan_atm.py

--- codegnan_atm.py
+++ codegnan_atm.py
@@ -1,7 +1,8 @@
-#account creation:
-print("Welcome to the Codegnan Bank  ")
+#---------------------------------------------------ACCOUNT CREATION ------------------------------------------------------------
+
+print("Welcome to the Codegnan Bank ...\n ")
 print("⭐"*40)
-n=int(input("No of Customers: "))
+n=int(input("\nNo of Customers: "))
 l=[]
 acc=1001
 for i in range(1,n+1):
@@ -21,30 +22,41 @@ for i in range(1,n+1):
         pin=None
     l.append([name,dob,bal,pin])
     print(f"-----Customer {i} Details Saved Successfully -----")
+    print(".........Exiting from the Account Creation..........")
     print("⭐"*40)
+    
     acc+=1
 accounts={}
+transaction_history={}
 acc=1001
 for i in l:
     accounts[acc]=i
+    transaction_history[acc]=[]
     acc+=1
-#operations
+    
+#------------------------------------------------OPERATIONS-----------------------------------------------------
+
 while True:
+#-----------------------------------------ATM CONSOLE -----------------------------------------------------
     print("⭐"*40)
-    print("Welcome to the Atm :")
+    print("\nWelcome to the Atm :\n")
     print("1. WITHDRAW")
     print("2. DEPOSIT")
     print("3. PIN GENERATION / RESET PIN")
     print("4. MINI STATEMENT")
-    print("5. EXIT")
+    print("5. Transaction History")
+    print("6. EXIT\n")
     print("⭐"*40)
-    ch=int(input("Enter Your Choice:"))
-    if ch!=5 and ch<5:
+#---------------------CHOICE-------------------------------------------------------------------------
+    ch=int(input("\nEnter Your Choice:"))
+    if ch!=6 and ch<6:
         ano=int(input("Enter the Account No [like 1001]:"))
         if ano not in accounts:
             print("\"Invalid Account Number \"")
             print("⭐"*40)
+            print()
             continue
+#---------------------WITHDRAW WINDOW-----------------------------------------------------------------
     if ch==1:
         if ano not in accounts:
             print("Invalid Account Number:")
@@ -56,9 +68,11 @@ while True:
                 agree=input("your account has NO PIN so we are redirecting you to the pin generation: Do you agree [yes(y)/no(n)]: ")
                 if agree.lower()in ["yes","y"]:
                     print("you are redirecting to main page........")
+                    print()
                     continue
                 else:
                     print("you are disagreed so we can't provide this account details.....back to main page")
+                    print()
                     continue
             if accounts[ano][-1]==None:
                 print("Your pin is not generated go for pin generation")
@@ -67,27 +81,48 @@ while True:
             else:
                 amt=float(input("Enter your amount:"))
                 if amt<=accounts[ano][-2]:
-                    print(" ---- Withdraw Successful !!!! ----")
+                    print()
+                    print(" ---- Withdraw Successful !!!! ----\n")
+
+                    print(f"Amount before withdraw : {accounts[ano][-2]}")
+                    transaction_history[ano].append("Amount before withdraw :"+str(accounts[ano][-2]))
+                    print(f"Amount Withdrawn: {amt}")
+                    transaction_history[ano].append("Amount Withdrawn:"+str(amt))
                     accounts[ano][-2]-=amt
                     print("Current Balance: ",accounts[ano][-2])
+                    transaction_history[ano].append("Balance: "+str(accounts[ano][-2]))
+                    
                 else:
                     print("\"Insufficient Funds Sir\"")
         print("..Exiting from the With-draw Window ...")
         
         print("⭐"*40)
+#----------------------DEPOSIT WINDOW---------------------------------------------------------------------------------
+        
     elif ch==2:
         deposit=float(input("Enter the deposit amount:"))
+        print()
+        print("---- Deposit Successful !!! ----\n")
+        print(f"Amount: {accounts[ano][-2]}")
+        transaction_history[ano].append("Amount: "+str(accounts[ano][-2]))
         accounts[ano][-2]+=deposit
-        print("---- Deposit Successful !!! ----")
+        print("Deposited Amount: ",deposit)
+        transaction_history[ano].append("Deposited Amount : "+str(deposit))
         print("Current Balance: ",accounts[ano][-2])
+        transaction_history[ano].append("Balance : "+str(accounts[ano][-2]))
         print("..Exiting from the Deposit Window ...")
         print("⭐"*40)
+
+#-----------------------PIN GENERATION & RESET WINDOW--------------------------------------------------------------
+    
     elif ch==3:
         print("Dear Customer !! Welcome to Pin Generation: ")
+        print()
         print("your acc no: ",ano)
         if accounts[ano][-1]==None:
             pin =int(input(f"Enter Your New pin {accounts[ano][0]} :"))
             accounts[ano][-1]=pin
+            print()
             print("Pin is generated successfully")
             print("...Exiting from the Pin generation window......")
         else:
@@ -98,33 +133,39 @@ while True:
                     print("\"Confirmed Old Pin\"")
                     pin=int(input("Enter New Pin Now: "))
                     accounts[ano][-1]=pin
+                    print()
                     print("Pin is generated successfully")
                     print(".. Exiting from the pin generation ...")
                     
                 else:
-                    print("..you entered wrong old pin so Exiting from the pin generation ...")
+                    
+                    print("\n..you entered wrong old pin so Exiting from the pin generation ...")
             else:
-                print("You disagreee to change the pin so back to main window ......")
+                
+                print("\nYou disagreee to change the pin so back to main window ......")
                 
         print("⭐"*40)
+
+#------------------------MINI STATEMENT WINDOW-------------------------------------------------------------------
+        
     elif ch==4:
-        print("Dear Customer!! Here is your Mini Statement")
+        print("Dear Customer!! Here is your Mini Statement....\n")
         try:
             pin=int(input("Enter the pin :"))
         except:
             agree=input("your account has NO PIN so we are redirecting you to the pin generation: Do you agree [yes(y)/no(n)]: ")
             if agree.lower()in ["yes","y"]:
-                print("you are redirecting to main page........")
+                print("\nyou are redirecting to main page........")
                 continue
             else:
-                print("you are disagreed so we can't provide this account details.....back to main page")
+                print("\nyou are disagreed so we can't provide this account details.....back to main page")
                 continue
             
                 
         if accounts[ano][-1]==None:
-            print("Your pin is not generated go for pin generation")
+            print("\nYour pin is not generated go for pin generation")
         elif accounts[ano][-1]!=pin:
-            print("Invalid Pin !!")
+            print("\nInvalid Pin !!")
         else:
             print("Account No: ",ano)
             print(f"Name : {accounts[ano][0]}")
@@ -136,13 +177,30 @@ while True:
             print(f"Balance: {accounts[ano][2]}")
         print("..Exiting from the Mini Statement window ...")
         print("⭐"*40)
-    elif ch>5:
+
+#-----------------------TRANSACTION HISTORY WINDOW----------------------------------------------------------------------------------
+        
+    elif ch==5:
+        print("Dear Customer!! Welcome to the Transaction History")
+        
+        if len(transaction_history[ano])==0:
+            print("\n----No Transactions Made-------")
+        else:
+            for i in transaction_history[ano]:
+                print(i)
+                
+        print("..Exiting the Transaction History ....")
+        print("⭐"*40)
+#-----------------------EXITING WINDOW --------------------------------------------------------------------------------------------
+        
+    elif ch>6:
         print("Invalid choice ")
         print("back to main window")
     else:
-        print("Exiting from the ATM Thank you!!  Dear Customer visit again!!")
+        print("\nExiting from the ATM Thank you!!  Dear Customer visit again!!")
         print("⭐"*40)
         break
+    print()
 
             
         
